8d8e3f1c82c2d786de54a7551e40bbeb228e0c77
kastashenkova <astashenkova.katya@gmail.com>
2026-08-14 12:37:39 +0300

    Resolve conflict

--- src/main/java/org/example/mapper/WineMapper.java
+++ src/main/java/org/example/mapper/WineMapper.java
@@ -1,4 +1,14 @@
-package org.example.mapper;
-
-public interface WineMapper {
-}
+//package org.example.mapper;
+//
+//import org.example.dto.wine.WineRequestDto;
+//import org.example.dto.wine.WineResponseDto;
+//import org.example.model.Wine;
+//import org.mapstruct.Mapping;
+//
+//public interface WineMapper {
+//    WineResponseDto toDto(Wine wine);
+//
+//    @Mapping(target = "id", ignore = true)
+//    @Mapping(target = "deleted", ignore = true)
+//    Wine toEntity(WineRequestDto requestDto);
+//}
