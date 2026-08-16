b95722cf7f9ab689b04b4ddbf89dd6a6c0165ba5
kastashenkova <astashenkova.katya@gmail.com>
2026-08-16 12:20:29 +0300

    add db migrations

--- src/main/java/org/example/dto/shopping_cart/ShoppingCartResponseDto.java
+++
@@ -1,4 +0,0 @@
-package org.example.dto.shopping_cart;
-
-public class ShoppingCartResponseDto {
-}
--- src/main/java/org/example/service/shopping_cart/ShoppingCartServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package org.example.service.shopping_cart;
-
-public class ShoppingCartServiceImpl implements ShoppingCartService {
-}
